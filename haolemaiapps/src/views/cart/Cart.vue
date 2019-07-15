640d554e511bc02aeefedffa2e9ffd9f71c7554a
jiarugithub <1218683935@qq.com>
2019-07-15 21:29:43 +0800

    ddd

--- haolemaiapps/src/views/cart/Cart.vue
+++ haolemaiapps/src/views/cart/Cart.vue
@@ -10,11 +10,15 @@
     <div class="er_">
      <img class="im" src="http://127.0.0.1:8095/img/cart/cart.png" style="width:150px;height:100px">
      <div class="kong">购物车还空着呢，快去挑选吧</div>
-     <router-link to="javascript:;" class="tiao_">去首页</router-link>
-     <router-link to="javascript:;" class="zhuan_">我的收藏</router-link>
-     <div class="weini_"><img src="http://127.0.0.1:8095/img/cart/xiexian.png" style="width:20px"><span>为你推荐</span><img src="http://127.0.0.1:8095/img/cart/xiexian.png" style="width:20px;margin-left:5px" ></div>
+     <a href="javascript:;" class="tiao">去首页</a>
+     <a href="javascript:;" class="zhuan_">我的收藏</a>
+     <div class="weini_">
+       <img src="http://127.0.0.1:8095/img/cart/xiexian.png" style="width:20px">
+       <span>为你推荐</span>
+       <img src="http://127.0.0.1:8095/img/cart/xiexian.png" style="width:20px;margin-left:5px" >
+     </div>
     </div>
-    <div></div>
+    
   </div>
 </template>
 
@@ -56,38 +60,42 @@ background-color:#f5f5f5;
  color:#999; 
 }
 .er_{
+  text-align:center;
  background:#f5f5f5;
- height:350px;
+  height:350px;
  margin-top:20px;
- position:relative;
+ position:relative; 
 }
-.tiao_{
-position:absolute;top:230px;left:70px;
+.tiao{
 text-decoration:none;
 color:red;
-display:block;
+display:inline-block;
 border:1px solid red;
+margin-top:20px;
 width:100px;
 height:50px;
 line-height:50px;
-transform:translate(0,-20px)
+
 }
 .zhuan_{
- position:absolute;top:230px;left:200px; 
+ 
  text-decoration:none;
  color:red;
  border:1px solid red;
+ display:inline-block;
+ margin-left:80px;
  width:100px;
  height:50px;
  line-height:50px;
- transform:translate(0,-20px)
+ 
 }
 .weini_{
- margin-top:110px;
+ margin-top:30px;
  height:50px; 
 }
 .weini_>span{
 display:inline-block;
-transform:translate(0,-10px)
+
+
 }
 </style>
