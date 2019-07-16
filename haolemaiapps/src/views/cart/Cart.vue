23b3a64e3518b3e51bd9d04fc9b3b98982f8b58a
jiarugithub <1218683935@qq.com>
2019-07-16 22:42:08 +0800

    11

--- haolemaiapps/src/views/cart/Cart.vue
+++ haolemaiapps/src/views/cart/Cart.vue
@@ -18,7 +18,14 @@
        <img src="http://127.0.0.1:8095/img/cart/xiexian.png" style="width:20px;margin-left:5px" >
      </div>
     </div>
-    
+    <!-- 图片鞋子商品 -->
+    <div>
+      <div v-for="(elem,i) of task" :key="i">
+        <img :src="{{elem.photo}}">
+         <p>{{elem.title}}</p>
+         <p>¥{{elem.price}}</p>
+      </div> 
+    </div>
   </div>
 </template>
 
@@ -26,7 +33,9 @@
 export default {
     data(){
         return {
-
+      task:[{photo:"http://127.0.0.1:8095/img/cart/487.jpg",title:"C|eef男式 满天星兵马俑侃爷椰子鞋男女情侣款高品质潮鞋飞织鞋",price:238},
+      {photo:"http://127.0.0.1:8095/img/cart/4871.jpg",title:"c|eef男式休闲鞋韩版低帮板鞋男鞋时尚皮鞋软面牛皮加布板鞋",price:278}
+      ]
         }
     }
 }
